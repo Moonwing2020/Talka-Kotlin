cc75cd1d162d34cc646eac41d008d6a8801ab155
Moonwing2020 <hxgame2020@qq.com>
2026-01-04 22:37:37 +0800

    ---

--- app/src/main/java/com/hjq/demo/talka/login/TalkaRegisterActivity.kt
+++ app/src/main/java/com/hjq/demo/talka/login/TalkaRegisterActivity.kt
@@ -1,8 +1,13 @@
 package com.hjq.demo.talka.login
 
 import android.net.Uri
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.core.net.toUri
 import com.bumptech.glide.load.MultiTransformation
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
@@ -19,6 +24,15 @@ import kotlin.getValue
 class TalkaRegisterActivity : AppActivity(){
 
     private val avatarView: ImageView? by lazyFindViewById(R.id.img)
+    private val sexRadioButton : RadioGroup? by lazyFindViewById(R.id.mSexRadioGroup)
+
+    private val sexText: TextView by lazyFindViewById(R.id.edt_sex)
+
+    private val nameEdit : EditText by lazyFindViewById(R.id.edit_nick_name)
+
+    var sexIndex : Int  = 1; //  1 男 2 女
+
+    private var nameStr : String = ""
 
     private var avatarUrl: Uri? = null
 
@@ -30,11 +44,47 @@ class TalkaRegisterActivity : AppActivity(){
         ImmersionBar.setTitleBar(this, findViewById(R.id.title))
 
         setOnClickListener(avatarView)
+
+        addEditTextListener()
+    }
+
+    private fun addEditTextListener() {
+        nameEdit.addTextChangedListener(object  : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                nameStr = s?.toString()?.trim() ?: ""
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+            }
+        })
     }
 
     override fun initData() {
+        sexRadioButton?.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.btnMale -> {
+                    sexIndex = 1
+                    sexText.setText(R.string.male)
+                }
+                R.id.btnFeMale->{
+                    sexIndex = 2
+                    sexText.setText(R.string.female)
+                }
+            }
+        }
     }
-
 
     @SingleClick
     override fun onClick(view: View) {
